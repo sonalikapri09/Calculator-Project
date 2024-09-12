5a65779922d63863346815e9f47328bf5aabf218
sonalikapri09 <kaprisonali09@gmail.com>
2024-09-12 23:07:25 +0530

    Add files via upload

---
+++ Calculator_Project/script.js
@@ -0,0 +1,34 @@
+const displayNum = document.querySelector(".input-text");
+const buttons = document.querySelectorAll("button");
+
+let string = " ";
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerText === "="){
+      // check if the string is not empty before evaluating
+      if (string.trim() !== ""){
+        try{
+           // Use eval to calculate the result
+        string = eval(string).toString(); // 
+        displayNum.value = string; // display update
+        }
+      catch(err){
+          displayNum.value = 'error';
+          string = "";
+      }
+    }
+  }
+     else if (e.target.innerText === "DEL") {
+      string = string.substring(0, string.length - 1); // remove last character
+      displayNum.value = string; // display update
+    }
+     else if (e.target.innerText === "AC") {
+      string = "";
+      displayNum.value = string; // clear the display
+    } 
+    else {
+      string += e.target.innerText;
+      displayNum.value = string; // Display the updated input
+    }
+  });
+});
